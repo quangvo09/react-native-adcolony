23b43b05ee96930c2a7f328cc93c4004e735a68b
Quang Vo <thanhquang2608@gmail.com>
2019-06-14 23:23:09 +0700

    Update

--- android/src/main/java/com/smartapp/rnadcolony/RNAdcolonyPackage.java
+++ android/src/main/java/com/smartapp/rnadcolony/RNAdcolonyPackage.java
@@ -10,12 +10,12 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-public class RNRnAdcolonyPackage implements ReactPackage {
+public class RNAdcolonyPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList();
       return Arrays.<NativeModule>asList(
-          new RNRnAdcolonyModule(reactContext),
+          new RNAdcolonyModule(reactContext),
           new RNAdColonyInterstitialAdModule(reactContext)
       );
     }
